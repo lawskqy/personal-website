b014bc7f2ae1f5f30ba073c004d686e8655946ab
lawskqy <radchenkoanastasiia2003@gmail.com>
2026-06-05 16:37:21 +0200

    added media for project, created classes for highlight styling, added footer

---
+++ script.js
@@ -0,0 +1,15 @@
+const openGithubProject = () => {
+    window.open("https://github.com/lawskqy/interactive-recipe");
+};
+
+const openGithub = () => {
+    window.open("https://github.com/lawskqy");
+};
+
+const openEmail = () => {
+    window.open("mailto:anastasiiaradchenko2003@gmail.com");
+};
+
+const openLinkedin = () => {
+    window.open("https://www.linkedin.com/");
+};
